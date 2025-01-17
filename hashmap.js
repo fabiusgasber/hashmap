66d0c216d809a0b64b8d4ab21a6a9df808adff2f
Fabius Gasber <fabiusgasber@gmail.com>
2025-01-17 11:20:16 +0100

    feat: add hash method to produce hash codes from keys

--- hashmap.js
+++ hashmap.js
@@ -9,4 +9,13 @@ class HashMap {
         this.capacity = this.buckets.length;
     }
 
-
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+     
+        return hashCode;
+    }
