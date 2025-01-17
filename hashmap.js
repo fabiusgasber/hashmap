c905bc2f6a6b213be497e8b67589c888d99463b1
Fabius Gasber <fabiusgasber@gmail.com>
2025-01-17 11:19:06 +0100

    feat: add hashmap class

--- hashmap.js
+++ hashmap.js
@@ -1,2 +1,12 @@
 import { LinkedList } from "./linkedlist.js";
 
+class HashMap {
+
+    loadFactor = 0.75;
+
+    constructor(capacity = 16){
+        this.buckets = new Array(capacity).fill(null);
+        this.capacity = this.buckets.length;
+    }
+
+
